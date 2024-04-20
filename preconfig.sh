0bdbdc9c5ea4725adc5467b1caecb4c6613d3139
jjlist <58911279+jjlist@users.noreply.github.com>
2024-04-20 13:59:26 +0300

    Update preconfig.sh

--- preconfig.sh
+++ preconfig.sh
@@ -3,14 +3,17 @@ echo =================
 lsblk
 echo =================
 parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart ESP fat32 1MB 513MB
+parted /dev/sda -- mkpart ESP fat32 1MiB 513MiB
 parted /dev/sda -- set 1 esp on
-parted /dev/sda -- mkpart root ext4 513MB 100% #-8GB
+parted /dev/sda -- mkpart home ext4 -300GiB 100%
+parted /dev/sda -- mkpart root ext4 513MiB 100% #-8GB
 #parted /dev/sda -- mkpart swap linux-swap -8GB 100%
 lsblk
 echo =================
-mkfs.ext4 -L nixos /dev/sda2
+mkfs.ext4 -L nixos /dev/sda3
 mount /dev/disk/by-label/nixos /mnt
+mkfs.ext4 -L home /dev/sda2
+mount /dev/disk/by-label/home /mnt/home
 #mkswap -L swap /dev/sda2
 #swapon /dev/sda2
 mkfs.fat -F 32 -n boot /dev/sda1
